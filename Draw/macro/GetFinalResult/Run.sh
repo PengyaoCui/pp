1b24c461bd08a3f2a1dd27f1f24f92f97e778315
Pengyao <pengyao.cui@cern.ch>
2020-10-15 17:54:41 +0800

    updated some code with new OC08

--- Draw/macro/GetFinalResult/Run.sh
+++ Draw/macro/GetFinalResult/Run.sh
@@ -8,3 +8,6 @@ sh runUEUncer.sh
 sh runTotUncer.sh
 sh runFinalSpect.sh
 sh runFinalRatio.sh
+
+cp result/pp.root /home/cuipengyao/study/pythia/NewModes/dPySjets_20200909_UpdateOmegabin/data/Data
+cp result/pp.root /home/cuipengyao/study/dPaperPlot/data 
