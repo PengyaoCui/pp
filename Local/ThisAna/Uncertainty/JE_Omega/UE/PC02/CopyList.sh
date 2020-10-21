0997ddd18c85aecd3b5c003bfeaf4390442f7fcd
Pengyao <pengyao.cui@cern.ch>
2020-10-21 19:27:28 +0800

    add PC02-08

---
+++ Local/ThisAna/Uncertainty/JE_Omega/UE/PC02/CopyList.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+for i in \
+'"./AnalysisOutputs_LoopJetRD.root"','"./AnalysisOutputs_LoopJetRD_back.root"','"listLoopJetRD_Kshort_Default_JC"' \
+'"./AnalysisOutputs_LoopJetRD.root"','"./AnalysisOutputs_LoopJetRD_back.root"','"listLoopJetRD_Lambda_Default_JC"' \
+'"./AnalysisOutputs_LoopJetRD.root"','"./AnalysisOutputs_LoopJetRD_back.root"','"listLoopJetRD_AntiLa_Default_JC"' \
+'"./AnalysisOutputs_LoopJetRD.root"','"./AnalysisOutputs_LoopJetRD_back.root"','"listLoopJetRD_Xi_Default_JC"' \
+'"./AnalysisOutputs_LoopJetRD.root"','"./AnalysisOutputs_LoopJetRD_back.root"','"listLoopJetRD_Omega_Default_JC"' \
+                                     
+do
+root -l -b -q CopyList.C\(${i}\)
+done
+#
+exit 0
